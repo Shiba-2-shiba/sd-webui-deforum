0b6a31317d52242628dcc9cf4134bb0ce183db77
Shiba*2 <135475967+Shiba-2-shiba@users.noreply.github.com>
2025-04-16 17:18:35 +0900

    Update ui_right.py

--- scripts/deforum_helpers/ui_right.py
+++ scripts/deforum_helpers/ui_right.py
@@ -98,7 +98,7 @@ def on_ui_tabs():
                     load_settings_btn = gr.Button('Load All Settings', elem_id='deforum_load_settings_btn')
                     load_video_settings_btn = gr.Button('Load Video Settings', elem_id='deforum_load_video_settings_btn')
 
-        component_list = [components[name] for name in get_component_names()]
+        component_list = [components[name] for name in get_component_names() if name in components]
 
         submit.click(
                     fn=wrap_gradio_gpu_call(run_deforum),
@@ -112,7 +112,7 @@ def on_ui_tabs():
                     ],
                 )
         
-        settings_component_list = [components[name] for name in get_settings_component_names()]
+        settings_component_list = [components[name] for name in get_settings_component_names() if name in components]
         video_settings_component_list = [components[name] for name in list(DeforumOutputArgs().keys())]
 
         save_settings_btn.click(
